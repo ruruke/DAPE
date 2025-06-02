e45070edde26a5500dcb5be9e042535b724f0811
Kisaragi Marine <kisaragi.effective@gmail.com>
2025-06-03 00:16:00 +0900

    wip

--- KisaragiTech.Dape/User/Database/UserRepositoryImpl.cs
+++ KisaragiTech.Dape/User/Database/UserRepositoryImpl.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Threading.Tasks;
+using KisaragiTech.Dape.User.Interface;
+using KisaragiTech.Dape.User.Model;
 using Neo4j.Driver;
 
 namespace KisaragiTech.Dape.User.Database;
@@ -18,10 +21,48 @@ internal sealed class UserRepositoryImpl
         
         return await session.ExecuteReadAsync(async tx =>
         {
-            // TODO: ORMてきなメソッドの方に置き換えたほうがいいかも
+            // language=cypher
             const string query = "MATCH (u:User { root: true }) RETURN u LIMIT 1";
             var cursor = await tx.RunAsync(query);
             return await cursor.FetchAsync();
+        });
+    }
+
+    public async Task<bool> FindUserByPreferredHandle(string preferredHandle)
+    {
+        await using var session = driver.AsyncSession(sess => sess.WithDefaultAccessMode(AccessMode.Read));
+
+        return await session.ExecuteReadAsync(async tx =>
+        {
+            // language=cypher
+            var query = "MATCH (n:Person { handle: $handle }) RETURN true AS exists LIMIT 1";
+            var cursor = await tx.RunAsync(query, new { handle = preferredHandle });
+
+            return await cursor.FetchAsync();
+        });
+    }
+
+    private async Task AssertPreferredHandleUniqueness(IUser user)
+    {
+        if (await this.FindUserByPreferredHandle(user.GetPreferredHandle()))
+        {
+            throw new ArgumentException($"すでに {user.GetPreferredHandle()} は登録されています。");
+        }
+    }
+
+    public async Task InsertUser(LocalRegisteredUser user)
+    {
+        await this.AssertPreferredHandleUniqueness(user);
+        
+        await using var session = driver.AsyncSession(sess => sess.WithDefaultAccessMode(AccessMode.Write));
+
+        await session.ExecuteWriteAsync(tx =>
+        {
+            // language=cypher
+            var query = "CREATE (n:Person { handle: $name, id: $id })";
+            var parameters = new { handle = user.GetPreferredHandle(), id = user.GetIdentifier().Raw.ToString() };
+
+            return tx.RunAsync(query);
         });
     }
 }
